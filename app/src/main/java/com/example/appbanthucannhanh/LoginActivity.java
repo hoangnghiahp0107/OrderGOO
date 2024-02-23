ea52b9c4930b34f79f7331b5f6ac0ade6ed180e9
hoangnghiahp0107 <hoangnghiahp0107@gmail.com>
2024-02-23 17:16:20 +0700

    23-02

---
+++ app/src/main/java/com/example/appbanthucannhanh/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.appbanthucannhanh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edtAccount, edtPassword;
+    Button btnLogin, btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        addControls();
+        addEvents();
+    }
+    private void addEvents(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = edtAccount.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+                if(account.isEmpty())
+                {
+                    edtAccount.setError("Vui lòng nhập tài khoản");
+                }
+                if(password.isEmpty())
+                {
+                    edtPassword.setError("Vui lòng nhập mật khẩu");
+                }
+                if(!account.isEmpty() && !password.isEmpty())
+                {
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+    private void addControls(){
+        edtAccount = findViewById(R.id.edtAccount);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+}
---
+++ app/src/main/java/com/example/appbanthucannhanh/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.appbanthucannhanh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.appbanthucannhanh.fragment.HomeFragment;
+import com.example.appbanthucannhanh.fragment.OrderFragment;
+import com.example.appbanthucannhanh.fragment.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+        if (savedInstanceState == null) {
+            // Nếu savedInstanceState rỗng, tức là activity được tạo lần đầu tiên, thì hiển thị HomeFragment
+            loadFragment(new HomeFragment());
+        }
+    }
+    private void loadFragment(Fragment fmNew){
+        FragmentTransaction fmTran = getSupportFragmentManager().beginTransaction();
+        fmTran.replace(R.id.main_frame, fmNew);
+        fmTran.addToBackStack(null);
+        fmTran.commit();
+    }
+    private void addEvents(){
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fmNew;
+                if(item.getItemId()==R.id.bottomHome){
+                    fmNew = new HomeFragment();
+                    loadFragment(fmNew);
+                    return true;
+                }
+                if(item.getItemId()==R.id.bottomOrder){
+                    fmNew = new OrderFragment();
+                    loadFragment(fmNew);
+                    return true;
+                }
+                if(item.getItemId()==R.id.bottomSetting){
+                    fmNew = new SettingFragment();
+                    loadFragment(fmNew);
+                    return true;
+                }
+                return true;
+            }
+        });
+    }
+    private void addControls(){
+        bottomNav = findViewById(R.id.bottomNav);
+    }
+}
